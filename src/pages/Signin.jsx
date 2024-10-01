a808b62e8f9b89f278b2333e4467e88b066da608
mak0551 <khanafroz1516@gmail.com>
2024-10-01 18:29:31 +0530

    adding loader

--- src/pages/Signin.jsx
+++ src/pages/Signin.jsx
@@ -1,62 +1,76 @@
 import React from "react";
 import { Link, useNavigate } from "react-router-dom";
-import { useState } from 'react';
-
+import { useState } from "react";
+import Spinner from "../components/Spinner";
 function Signin() {
-    const navigate = useNavigate();
-    const [formData, setFormData] = useState({
-        email: '',
-        password: ''
-    });
-    // or another way
-    // const [email, setEmail] = useState('');
-    // const [password, setPassword] = useState('');
+  const navigate = useNavigate();
+  const [formData, setFormData] = useState({
+    email: "",
+    password: "",
+  });
+  // or another way
+  // const [email, setEmail] = useState('');
+  // const [password, setPassword] = useState('');
 
-    const handleChange = (e) => {
-        setFormData({ ...formData, [e.target.name]: e.target.value });
-    };
+  const [loading, setLoading] = useState(false);
+  const handleChange = (e) => {
+    setFormData({ ...formData, [e.target.name]: e.target.value });
+  };
 
-    const handleSubmit = async (e) => {
-        e.preventDefault();
-        try {
-            const response = await fetch('https://bookstore-backend-t7gr.onrender.com/api/v1/users/signin', {
-                method: 'POST',
-                headers: {
-                    'Content-Type': 'application/json'
-                },
-                body: JSON.stringify(formData)
-            });
-
-            const result = await response.json();
-            if (response.ok) {
-                alert('Signin successful!');
-                // Store JWT token in localStorage (or sessionStorage)
-                localStorage.setItem('token', result.token);
-                console.log(result.token)
-                // Navigate to the homepage or dashboard after signin
-                navigate('/home')
-            } else {
-                alert(result.message);
-            }
-        } catch (error) {
-            alert('Error during signin');
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    setLoading(true);
+    try {
+      const response = await fetch(
+        "https://bookstore-backend-t7gr.onrender.com/api/v1/users/signin",
+        {
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json",
+          },
+          body: JSON.stringify(formData),
         }
-    };
+      );
+
+      const result = await response.json();
+      if (response.ok) {
+        alert("Signin successful!");
+        // Store JWT token in localStorage (or sessionStorage)
+        localStorage.setItem("token", result.token);
+        console.log(result.token);
+        // Navigate to the homepage or dashboard after signin
+        navigate("/home");
+      } else {
+        alert(result.message);
+      }
+    } catch (error) {
+      setLoading(false);
+      alert("Error during signin");
+    }
+  };
   return (
     <div className="flex w-[100%] h-[100vh] bg-zinc-100 justify-center items-center">
-        <form onSubmit={handleSubmit} className="border border-[1.5px] border-zinc-400 bg-white flex flex-col p-12 max-md:p-2 max-md:py-16 rounded-xl items-center justify-center">
+      {loading ? (
+        <Spinner />
+      ) : (
+        <form
+          onSubmit={handleSubmit}
+          className="border border-[1.5px] border-zinc-400 bg-white flex flex-col p-12 max-md:p-2 max-md:py-16 rounded-xl items-center justify-center"
+        >
           <input
             type="email"
             name="email"
             placeholder="Email or username"
-            onChange={handleChange} required
+            onChange={handleChange}
+            required
             className="border-[1px] border-zinc-500 bg-zinc-200 font-mono px-2 my-1 rounded-mg h-10 w-[300px]"
           />
           <input
             type="password"
             name="password"
             placeholder="Password"
-            onChange={handleChange} required
+            onChange={handleChange}
+            required
             className="border-[1px] border-zinc-500 bg-zinc-200 font-mono px-2 my-1 rounded-mg h-10 w-[300px]"
           />
           <button
@@ -72,6 +86,7 @@ function Signin() {
             </Link>
           </span>
         </form>
+      )}
     </div>
   );
 }
--- src/pages/Signup.jsx
+++ src/pages/Signup.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import { Link, useNavigate } from "react-router-dom";
 import { useState } from "react";
+import Spinner from "../components/Spinner";
 
 function Signup() {
   const navigate = useNavigate();
@@ -15,8 +16,10 @@ function Signup() {
     setFormData({ ...formData, [e.target.name]: e.target.value });
   };
 
+  const [loading, setLoading] = useState(false);
   const handleSubmit = async (e) => {
     e.preventDefault();
+    setLoading(true);
     try {
       const response = await fetch(
         "https://bookstore-backend-t7gr.onrender.com/api/v1/users/signup",
@@ -38,61 +41,66 @@ function Signup() {
         alert(result.message);
       }
     } catch (error) {
+      setLoading(false);
       alert("Error during signup");
     }
   };
 
   return (
     <div className="flex w-[100%] h-[100vh] bg-zinc-100 justify-center items-center">
-      <form
-        onSubmit={handleSubmit}
-        className="border border-[1.5px] border-zinc-400 bg-white flex flex-col p-12 max-md:p-4 max-md:py-16 rounded-xl items-center justify-center"
-      >
-        <input
-          type="text"
-          name="firstname"
-          placeholder="Firstname"
-          className="border-[1px] border-zinc-500 bg-zinc-200 font-mono px-2 my-0.5 rounded-mg h-10 w-[280px]"
-          onChange={handleChange}
-          required
-        />
-        <input
-          type="text"
-          name="lastname"
-          placeholder="Lastname"
-          className="border-[1px] border-zinc-500 bg-zinc-200 font-mono px-2 my-0.5 rounded-mg h-10 w-[280px]"
-          onChange={handleChange}
-          required
-        />
-        <input
-          type="email"
-          name="email"
-          placeholder="Email"
-          className="border-[1px] border-zinc-500 bg-zinc-200 font-mono px-2 my-0.5 rounded-mg h-10 w-[280px]"
-          onChange={handleChange}
-          required
-        />
-        <input
-          type="password"
-          name="password"
-          placeholder="Password"
-          className="border-[1px] border-zinc-500 bg-zinc-200 font-mono px-2 my-0.5 rounded-mg h-10 w-[280px]"
-          onChange={handleChange}
-          required
-        />
-        <button
-          type="submit"
-          className="my-5 bg-blue-400 hover:bg-blue-500 text-white font-mono font-medium py-2 px-4 w-[280px] rounded-md tracking-tighter"
+      {loading ? (
+        <Spinner />
+      ) : (
+        <form
+          onSubmit={handleSubmit}
+          className="border border-[1.5px] border-zinc-400 bg-white flex flex-col p-12 max-md:p-4 max-md:py-16 rounded-xl items-center justify-center"
         >
-          Sign up
-        </button>
-        <span className="font-mono tracking-tighter	">
-          already have an account?{" "}
-          <Link to={"/signin"} className="text-red-900">
-            SignIn
-          </Link>
-        </span>
-      </form>
+          <input
+            type="text"
+            name="firstname"
+            placeholder="Firstname"
+            className="border-[1px] border-zinc-500 bg-zinc-200 font-mono px-2 my-0.5 rounded-mg h-10 w-[280px]"
+            onChange={handleChange}
+            required
+          />
+          <input
+            type="text"
+            name="lastname"
+            placeholder="Lastname"
+            className="border-[1px] border-zinc-500 bg-zinc-200 font-mono px-2 my-0.5 rounded-mg h-10 w-[280px]"
+            onChange={handleChange}
+            required
+          />
+          <input
+            type="email"
+            name="email"
+            placeholder="Email"
+            className="border-[1px] border-zinc-500 bg-zinc-200 font-mono px-2 my-0.5 rounded-mg h-10 w-[280px]"
+            onChange={handleChange}
+            required
+          />
+          <input
+            type="password"
+            name="password"
+            placeholder="Password"
+            className="border-[1px] border-zinc-500 bg-zinc-200 font-mono px-2 my-0.5 rounded-mg h-10 w-[280px]"
+            onChange={handleChange}
+            required
+          />
+          <button
+            type="submit"
+            className="my-5 bg-blue-400 hover:bg-blue-500 text-white font-mono font-medium py-2 px-4 w-[280px] rounded-md tracking-tighter"
+          >
+            Sign up
+          </button>
+          <span className="font-mono tracking-tighter	">
+            already have an account?{" "}
+            <Link to={"/signin"} className="text-red-900">
+              SignIn
+            </Link>
+          </span>
+        </form>
+      )}
     </div>
   );
 }
